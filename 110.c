1238c99d4c37f6ee71140533887da97b39d5f0c0
Pankaj singh <pankajsingh827928@gmail.com>
2025-11-27 22:51:40 +0530

    Update 110.c

--- 110.c
+++ 110.c
@@ -1,4 +1,4 @@
-// Write a program to take an integer array arr and an integer k as inputs. The task is to find the maximum element in each subarray of size k moving from left to right. Print the maximum elements for each window separated by spaces as output.
+/* Write a program to take an integer array arr and an integer k as inputs. The task is to find the maximum element in each subarray of size k moving from left to right. Print the maximum elements for each window separated by spaces as output.
 
 /*
 Sample Test Cases:
@@ -48,3 +48,4 @@ int main() {
     printf("\n");
     return 0;
 }
+
